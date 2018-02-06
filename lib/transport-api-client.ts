837aad69128660b4facf79eebfaff23f979b6f64
robingoeppert <robin.goeppert@gmail.com>
2018-02-06 20:18:36 +0100

    First methods declared

--- lib/transport-api-client.ts
+++ lib/transport-api-client.ts
@@ -1,3 +1,18 @@
+import {Location} from './models/location';
+import {Connection} from './models/connection';
+import {Journey} from './models/journey';
+
+
 export class TransportApiClient {
-    
+    public getLocations(): Array<Location> {
+        return [];
+    }
+
+    public getConnections(): Array<Connection> {
+        return [];
+    }
+
+    public getStationboard(): Array<Journey> {
+        return [];
+    }
 }
