11d98ddc89e32bc9b72aef7d8dcfa34d86138ec2
robingoeppert <robin.goeppert@gmail.com>
2018-02-09 20:38:18 +0100

    Module exports now the enums

--- dist/index.js
+++ dist/index.js
@@ -1,4 +1,10 @@
 "use strict";
 Object.defineProperty(exports, "__esModule", { value: true });
 var transport_api_client_1 = require("./lib/transport-api-client");
-module.exports = new transport_api_client_1.TransportApiClient();
+var location_type_1 = require("./lib/enums/location-type");
+var transportation_type_1 = require("./lib/enums/transportation-type");
+module.exports = {
+    TransportApiClient: transport_api_client_1.TransportApiClient,
+    LocationType: location_type_1.LocationType,
+    TransportationType: transportation_type_1.TransportationType
+};
--- index.ts
+++ index.ts
@@ -1,3 +1,9 @@
 import {TransportApiClient} from './lib/transport-api-client';
+import {LocationType} from './lib/enums/location-type';
+import {TransportationType} from './lib/enums/transportation-type';
 
-module.exports = new TransportApiClient();
+module.exports = {
+    TransportApiClient,
+    LocationType,
+    TransportationType
+};
