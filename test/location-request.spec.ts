6f2a5b21d6d96f9c971b10d1c48873aec134e16e
robingoeppert <robin.goeppert@gmail.com>
2018-02-19 17:21:17 +0100

    location request test completed

--- test/location-request.spec.ts
+++ test/location-request.spec.ts
@@ -8,31 +8,66 @@ import {Location} from '../lib/objects/location';
 
 describe('LocationRequest', () => {
     describe('Initializing', () => {
-        it('has set base URL and API route', () => {
-            const locationRequest: LocationRequest = new LocationRequest();
+        it('has set URL for byName constructor', () => {
+            const locationRequest: LocationRequest = LocationRequest.byName('Test');
             const url: string = locationRequest.getUrl();
             expect(url).to.be.a('string');
-            expect(url).to.be.equal('http://transport.opendata.ch/v1/locations');
+            expect(url).to.be.equal('http://transport.opendata.ch/v1/locations?query=Test');
+        });
+
+        it('has set URL for byCoordinates constructor', () => {
+            const locationRequest: LocationRequest = LocationRequest.byCoordinates(45.332, 8.79);
+            const url: string = locationRequest.getUrl();
+            expect(url).to.be.a('string');
+            expect(url).to.be.equal('http://transport.opendata.ch/v1/locations?x=45.332&y=8.79');
+        });
+    });
+
+    describe('Parameterization', () => {
+        it('has param "type" set correctly', () => {
+            const url: string = LocationRequest
+                .byName('Rafz')
+                .ofType(LocationType.POINT_OF_INTEREST)
+                .getUrl();
+
+            expect(url).to.be.equal('http://transport.opendata.ch/v1/locations?query=Rafz&type=poi');
+        });
+
+        it('has param "transportations" set correctly', () => {
+            const url: string = LocationRequest
+                .byCoordinates(45.332, 8.79)
+                .withTransports(TransportationType.BUS, TransportationType.CABLEWAY)
+                .getUrl();
+
+            expect(url).to.be.equal('http://transport.opendata.ch/v1/locations?x=45.332&y=8.79&transportations[]=bus&transportations[]=cableway');
         });
     });
 
     describe('Valid request by name', () => {
-        const locationRequest: LocationRequest = new LocationRequest()
-            .byName('Rafz')
-            .ofType(LocationType.POINT_OF_INTEREST)
-            .withTransports(TransportationType.BUS, TransportationType.S_SN_R);
-
-        it('has assembled required URL components', () => {
-            const url: string = locationRequest.getUrl();
-
-            expect(url).to.be.equal('http://transport.opendata.ch/v1/locations?query=Rafz&type=poi&transportations[]=bus&transportations[]=s_sn_r');
+        it('responds as expected', () => {
+            LocationRequest
+                .byName('Rafz')
+                .ofType(LocationType.POINT_OF_INTEREST)
+                .send()
+                .then(response => {
+                    expect(response).to.be.an('array');
+                    expect(response[0].id).not.to.be.undefined;
+                    expect(response[0].name).not.to.be.undefined;
+                });
         });
+    });
 
-        it('responds as expected', async () => {
-            const response: Location[] = await locationRequest.send();
-            expect(response).to.be.an('array');
-            expect(response[0].id).not.to.be.undefined;
-            expect(response[0].name).not.to.be.undefined;
+    describe('Valid request by coordinates', () => {
+        it('responds as expected', () => {
+            LocationRequest
+                .byCoordinates(45.332, 8.79)
+                .withTransports(TransportationType.BUS, TransportationType.S_SN_R)
+                .send()
+                .then(response => {
+                    expect(response).to.be.an('array');
+                    expect(response[0].id).not.to.be.undefined;
+                    expect(response[0].name).not.to.be.undefined;
+                });
         });
     });
 });
