fd7388842c2d2aa2016e327c7c24861560b65029
robingoeppert <robin.goeppert@gmail.com>
2018-02-24 11:40:44 +0100

    Two type enums exported

--- dist/index.js
+++ dist/index.js
@@ -3,8 +3,12 @@ Object.defineProperty(exports, "__esModule", { value: true });
 var transport_api_client_1 = require("./lib/transport-api-client");
 var location_type_1 = require("./lib/enums/location-type");
 var transportation_type_1 = require("./lib/enums/transportation-type");
+var stationboard_type_1 = require("./lib/enums/stationboard-type");
+var accessibility_type_1 = require("./lib/enums/accessibility-type");
 module.exports = {
     TransportApiClient: transport_api_client_1.TransportApiClient,
     LocationType: location_type_1.LocationType,
-    TransportationType: transportation_type_1.TransportationType
+    TransportationType: transportation_type_1.TransportationType,
+    StationboardType: stationboard_type_1.StationboardType,
+    AccessibilityType: accessibility_type_1.AccessibilityType
 };
--- index.ts
+++ index.ts
@@ -1,9 +1,14 @@
 import {TransportApiClient} from './lib/transport-api-client';
 import {LocationType} from './lib/enums/location-type';
 import {TransportationType} from './lib/enums/transportation-type';
+import {StationboardType} from './lib/enums/stationboard-type';
+import {AccessibilityType} from './lib/enums/accessibility-type';
 
 module.exports = {
     TransportApiClient,
+
     LocationType,
-    TransportationType
+    TransportationType,
+    StationboardType,
+    AccessibilityType
 };
